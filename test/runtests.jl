fff07dc68f4e5e7cbcf05e2ff3d5a049393534ca
arturgower <arturgower@gmail.com>
2021-08-09 15:07:29 +0100

    ../ typo

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,4 +6,4 @@ include("image-to-distortions.jl")
 
 # process some data
 include("../track_to_cabin_movement.jl")
-track_to_cabin_movement("../data/output_results_centre_line.csv",../"data/output.csv")
+track_to_cabin_movement("../data/output_results_centre_line.csv","../data/output.csv")
