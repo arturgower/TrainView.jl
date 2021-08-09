01682a7cc99109778407861f8a74d23665e52569
arturgower <arturgower@gmail.com>
2021-08-09 13:58:18 +0100

    add processing to tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,8 @@ using Test, TrainView
 using LinearAlgebra, Statistics
 
 include("track-to-image.jl")
-
 include("image-to-distortions.jl")
+
+# process some data
+include("../track_to_cabin_movement.jl")
+track_to_cabin_movement("../data/output_results_centre_line.csv",../"data/output.csv")
